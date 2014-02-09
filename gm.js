f8a693694b68bbe1aa9499f0f9390fb8dff9eee6
cyveros <rynoxo@gmail.com>
2014-02-09 11:46:56 -0500

    rework dataProvider

--- gm.js
+++ gm.js
@@ -57,7 +57,7 @@ var utils = {
     pagination: {
         create: function(selector) {
             var pages = $(selector);
-            var pagination = '<ul class="pagination">';
+            var pagination = '<ul class="pagination pagination-sm container">';
         
             $.each(pages, function(){
                 var tag = $(this).prop('tagName');
@@ -97,6 +97,8 @@ function Page() {
         inPage: false
     };
 
+    this.db = self.settings.data;
+
     this.load = {
         css: function(url) {
             $('head').append('<link rel="stylesheet" href="' + url + '" type="text/css"/>');
@@ -108,7 +110,7 @@ function Page() {
             $('body').append('<div id="modalContainer" class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h4 class="modal-title">Modal title</h4></div><div class="modal-body"><p>One fine body&hellip;</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Get Torrent</button></div></div></div></div>');
         },
         navbar: function() {
-            $('body').prepend('<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"><div class="container-fluid"><div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="#">第一会所</a></div><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"><ul class="nav navbar-nav"><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">板块 <b class="caret"></b></a><ul class="dropdown-menu"><li><a href="forumdisplay.php?fid=519">原创培训</a></li><li><a href="forumdisplay.php?fid=143">亚无原创</a></li><li><a href="forumdisplay.php?fid=25">亚无转贴</a></li><li><a href="forumdisplay.php?fid=230">亚有原创</a></li><li><a href="forumdisplay.php?fid=58">亚有转贴</a></li><li><a href="forumdisplay.php?fid=229">欧无原创</a></li><li><a href="forumdisplay.php?fid=77">欧无转贴</a></li><li><a href="forumdisplay.php?fid=231">动漫原创</a></li><li><a href="forumdisplay.php?fid=27">动漫转贴</a></li><li><a href="forumdisplay.php?fid=406">新手原创</a></li><li><a href="forumdisplay.php?fid=394">分流宣传</a></li><li><a href="forumdisplay.php?fid=530">自拍原创</a></li></ul></li><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">分类 <b class="caret"></b></a><ul id="categories" class="dropdown-menu"></ul></li></ul><ul class="nav navbar-nav navbar-right"><li id="navPag"><a href="#"></a></li><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a><ul class="dropdown-menu"><li><a href="#">Action</a></li><li><a href="#">Another action</a></li><li><a href="#">Something else here</a></li><li class="divider"></li><li><a href="#">Separated link</a></li></ul></li></ul></div></div></nav>');
+            $('body').prepend('<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"><div class="container-fluid"><div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="#">第一会所</a></div><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"><ul class="nav navbar-nav"><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">板块 <b class="caret"></b></a><ul class="dropdown-menu"><li><a href="forumdisplay.php?fid=519">原创培训</a></li><li><a href="forumdisplay.php?fid=143">亚无原创</a></li><li><a href="forumdisplay.php?fid=25">亚无转贴</a></li><li><a href="forumdisplay.php?fid=230">亚有原创</a></li><li><a href="forumdisplay.php?fid=58">亚有转贴</a></li><li><a href="forumdisplay.php?fid=229">欧无原创</a></li><li><a href="forumdisplay.php?fid=77">欧无转贴</a></li><li><a href="forumdisplay.php?fid=231">动漫原创</a></li><li><a href="forumdisplay.php?fid=27">动漫转贴</a></li><li><a href="forumdisplay.php?fid=406">新手原创</a></li><li><a href="forumdisplay.php?fid=394">分流宣传</a></li><li><a href="forumdisplay.php?fid=530">自拍原创</a></li></ul></li><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">分类 <b class="caret"></b></a><ul id="categories" class="dropdown-menu"></ul></li><li id="headerPaginator"></li></ul><ul class="nav navbar-nav navbar-right"><li id="navPag"><a href="#"></a></li><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a><ul class="dropdown-menu"><li><a href="#">Action</a></li><li><a href="#">Another action</a></li><li><a href="#">Something else here</a></li><li class="divider"></li><li><a href="#">Separated link</a></li></ul></li></ul></div></div></nav>');
         },
         includeCSS: function(resource) {
             var css = GM_getResourceText(resource);
@@ -134,7 +136,6 @@ function Page() {
             $('#headsearch, #ad_text, .portalbox, #header, #menu, #headfilter, #footfilter, .legend, #footercontainer').remove();
             $('#newspecial, #newspecialtmp').remove();
             $('table').addClass('table');
-            $('#wrapper').addClass('container');
         },
         list: function(){
             self.load.navbar();
@@ -147,6 +148,7 @@ function Page() {
             $('.folder, .icon, .lastpost, .category, tr .nums:not(:last-child), cite, .hot label, .new label, .lock label, .common label').remove();
             
             $('#wrapper').prepend(utils.pagination.create('.pages > *'));
+            //$('#headerPaginator').html(utils.pagination.create('.pages > *'));
             $('#wrapper div:first-child').remove();
 
             $('.pages_btns').addClass('container');
@@ -175,8 +177,8 @@ function Page() {
                         date: $(this).find('td').text()
                     };
 
-                    self.settings.data.push('articles', article);
-                    self.settings.data.push('categories', {id: article.categoryId, name: article.categoryName});
+                    self.db.collection('articles', article);
+                    self.db.collection('categories', {id: article.categoryId, name: article.categoryName});
 
                     tbodyContainer += utils.nano('<tr><td><a href="forumdisplay.php?fid=230&filter=type&typeid={categoryId}" class="badge">{categoryName}</a><a href="thread-{id}-1-1.html">{title}</a></td><td><span class="label label-success">{date}</span></td></tr>', article);    
                 }
@@ -201,7 +203,7 @@ function Page() {
             $('#newspecial_menu').remove();
             $('form[name="modactions"] .mainbox:not(:first)').remove();
             $('#foruminfo').remove();
-            $('#wrapper > ul').remove();
+            $('.modal-body  #wrapper > ul').remove();
 
             if (self.settings.inPage)
                 $('.modal-body .pages_btns').remove();
@@ -277,7 +279,7 @@ function Page() {
             }
         },
         category: function() {
-            var categories = self.settings.data.find('categories'),
+            var categories = self.db.find('categories'),
                 output = '';
 
             $.each(categories, function() {
@@ -329,22 +331,52 @@ function DataProvider() {
     var self = this;
 
     this.settings = {
-        data: {}
+        data: {
+
+        },
+        registry: {
+        	collection: {},
+        	collectionCount: 0
+        }
     };
 
     this.push = function(key, value) {
-        if (typeof(value) === 'object') {
-            if (self.find(key)) {
-                if ( ! self.search(key, 'id', value.id))
-                    self.settings.data[key].push(value);
-            } else {
-                self.settings.data[key] = [value];
+        var tmp = {};
+        tmp[key] = value;
+        self.settings.data = $.extend(self.settings.data, tmp);
+    };
+
+    this.collection = function(name, value) {
+    	if (self.hasCollection(name)) {
+            if ( ! self.hasCollectionEntry(name, value)) {
+                self.settings.data[name].push(value);
+                self.settings.registry.collection[name].entry += 1;
+                // for fast index search
+                self.settings.registry.collection[name].ids.push(value.id);
             }
         } else {
-            var tmp = {};
-            tmp[key] = value;
-            self.settings.data = $.extend(self.settings.data, tmp);
+            self.addCollection(name);
+            self.collection(name, value);
         }
+    };
+
+    this.hasCollection = function(name) {
+    	return self.settings.registry.collection.hasOwnProperty(name);
+    };
+
+    this.hasCollectionEntry = function(name, value) {
+    	return (self.settings.registry.collection[name].ids.indexOf(value.id) >= 0);
+    };
+
+    this.addCollection = function(name) {
+    	self.settings.registry.collection[name] = {
+    		entry: 0,
+    		schema: [],
+    		ids: [] 
+    	};
+
+    	self.settings.registry.collectionCount += 1;
+    	self.settings.data[name] = [];
     };
 
     this.void = function(key) {
@@ -394,5 +426,6 @@ function DataProvider() {
 
     this.all = function(){
         console.log(self.settings.data);
+        console.log(self.settings.registry);
     };
 }
