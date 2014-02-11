b3d1ef0c746e284ba8a83b74e1cd22acf959a0fd
cyveros <rynoxo@gmail.com>
2014-02-11 00:45:23 -0500

    feature session storage and cache browsing

--- gm.js
+++ gm.js
@@ -112,6 +112,10 @@ function Page() {
         navbar: function() {
             $('body').prepend('<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"><div class="container-fluid"><div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="#">第一会所</a></div><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"><ul class="nav navbar-nav"><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">板块 <b class="caret"></b></a><ul class="dropdown-menu"><li><a href="forumdisplay.php?fid=519">原创培训</a></li><li><a href="forumdisplay.php?fid=143">亚无原创</a></li><li><a href="forumdisplay.php?fid=25">亚无转贴</a></li><li><a href="forumdisplay.php?fid=230">亚有原创</a></li><li><a href="forumdisplay.php?fid=58">亚有转贴</a></li><li><a href="forumdisplay.php?fid=229">欧无原创</a></li><li><a href="forumdisplay.php?fid=77">欧无转贴</a></li><li><a href="forumdisplay.php?fid=231">动漫原创</a></li><li><a href="forumdisplay.php?fid=27">动漫转贴</a></li><li><a href="forumdisplay.php?fid=406">新手原创</a></li><li><a href="forumdisplay.php?fid=394">分流宣传</a></li><li><a href="forumdisplay.php?fid=530">自拍原创</a></li></ul></li><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">分类 <b class="caret"></b></a><ul id="categories" class="dropdown-menu"></ul></li><li id="headerPaginator"></li></ul><ul class="nav navbar-nav navbar-right"><li id="navPag"><a href="#"></a></li><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a><ul class="dropdown-menu"><li><a href="#">Action</a></li><li><a href="#">Another action</a></li><li><a href="#">Something else here</a></li><li class="divider"></li><li><a href="#">Separated link</a></li></ul></li></ul></div></div></nav>');
         },
+        footer: function() {
+        	$('body').append('<div id="footer"><div class="container block-center"><p class="text-muted">asdasdasdas asdasd</p></div></div>');
+        	$('style').append('#footer {bottom:0;width:100%;position:fixed;}');
+        },
         includeCSS: function(resource) {
             var css = GM_getResourceText(resource);
             $('body').append('<style>' + css + '</style>');
@@ -131,71 +135,94 @@ function Page() {
         return route;
     };
 
+    this.extractList = function(tbody) {
+    	var threadId, category, article;
+
+    	$.each(tbody, function(){
+            if ($(this).html() != '') {
+                threadId = $(this).prop('id').replace('normalthread_', '');
+                category = $(this).find('th > em a'),
+                article = {
+                    id: threadId,
+                    title: $(this).find('th > span:eq(0) a').text(),
+                    categoryId: category.length ? category.attr('href').match(/typeid=([^&]+)/)[1] : 0,
+                    categoryName: category.length ? category.text() : 0,
+                    date: $(this).find('td').text(),
+                    content: ''
+                };
+
+                self.db.collection('articles', article);
+                
+                if (category.length) {
+                	self.db.collection('categories', {id: article.categoryId, name: article.categoryName});
+                }
+            }
+        });
+    };
+
     this.tinker = {
         common: function(){
             $('#headsearch, #ad_text, .portalbox, #header, #menu, #headfilter, #footfilter, .legend, #footercontainer').remove();
             $('#newspecial, #newspecialtmp').remove();
             $('table').addClass('table');
         },
+        repost: function() {
+    		$('#wrap').html('');
+    		var output = '';
+
+    		var articles = self.db.find('articles');
+
+    		$.each(articles, function() {
+    			var article = $.parseJSON(sessionStorage.getItem('articles' + this.id));
+
+    			if (article.content != '') {
+    				var src = $(article.content).find('img:eq(0)').attr('src');
+    				console.log(src);
+    				this.tmp = '<img src="' + src + '" style="width:265px;"/>';
+
+					output += utils.nano('<div class="panel panel-default" data-article-id="{id}"><div class="panel-heading"><a href="forumdisplay.php?fid=230&filter=type&typeid={categoryId}" class="badge">{categoryName}</a></div><div class="panel-body"><a data-article-id="{id}" href="thread-{id}-1-1.html">{title}</a>{tmp}</div><div class="panel-footer">{date}</div></div>', this);
+
+    			} else {
+					output += utils.nano('<div class="panel panel-default" data-article-id="{id}"><div class="panel-heading"><a href="forumdisplay.php?fid=230&filter=type&typeid={categoryId}" class="badge">{categoryName}</a></div><div class="panel-body"><a data-article-id="{id}" href="thread-{id}-1-1.html">{title}</a></div><div class="panel-footer">{date}</div></div>', this);
+    			}
+       		});
+
+    		$('#wrap').append(output).append('<div id="push></div>"');
+    		$('style').append('.panel {width:290px;position:absolute;} .panel img {width:270px;height:auto;} #wrap {position:relative;margin:80px auto;}');
+    		$('.panel').byllboard();
+        },
         list: function(){
+
+            var pagination = utils.pagination.create('.pages:eq(0) > *');
+            var tbody = $('form table:last-child tbody[id]');
+
+            self.extractList(tbody);
+            
+            $('body').html('');
+            
+            self.load.includeCSS('bootstrapcss');
             self.load.navbar();
-            $('#wrapper > div:not(.threadlist, :first-child)').remove();
-            $('#wrapper > ul').remove();
-            $('.threadlist table:not(:last-child)').remove();
-            $('#foruminfo').remove();
-            $('h1, .headactions').remove();
-            $('.pages:eq(1), #newspecial_menu').remove();
-            $('.folder, .icon, .lastpost, .category, tr .nums:not(:last-child), cite, .hot label, .new label, .lock label, .common label').remove();
-            
-            $('#wrapper').prepend(utils.pagination.create('.pages > *'));
-            //$('#headerPaginator').html(utils.pagination.create('.pages > *'));
-            $('#wrapper div:first-child').remove();
+            $('body').append('<div id="wrap" class="container"></div>');
 
-            $('.pages_btns').addClass('container');
-            
-            $('.threadlist').html($('form').html());
-            var content = $('.mainbox').clone();
-            $('.threadlist').remove();
-            
-            $('#wrapper').css({'padding-top': '80px'}).append(content);
-            
-            var tbody = $('tbody');
-            var tbodyContainer = '<tbody>';
-
-            var threadId, category, article;
-            
-            $.each(tbody, function(){
-                if ($(this).html() != '') {
-                    threadId = $(this).prop('id').replace('normalthread_', '');
-                    category = $(this).find('th > em a'),
-
-                    article = {
-                        id: threadId,
-                        title: $(this).find('th > span:eq(0) a').text(),
-                        categoryId: category.prop('href').match(/typeid=([^&]+)/)[1],
-                        categoryName: category.text(),
-                        date: $(this).find('td').text()
-                    };
-
-                    self.db.collection('articles', article);
-                    self.db.collection('categories', {id: article.categoryId, name: article.categoryName});
-
-                    tbodyContainer += utils.nano('<tr><td><a href="forumdisplay.php?fid=230&filter=type&typeid={categoryId}" class="badge">{categoryName}</a><a href="thread-{id}-1-1.html">{title}</a></td><td><span class="label label-success">{date}</span></td></tr>', article);    
-                }
-            });
-
-            tbodyContainer += '</tbody>';
-            
-            $('.table').html(tbodyContainer);
-
+            self.tinker.repost();
             self.tinker.category();
 
             self.load.modal();
+			self.load.footer();
+            $('#footer .container').html(pagination);
 
-            $(document).on('click', '.threadlist a:not(.badge)', function(e){
+            $(document).on('click', '.panel a:not(.badge)', function(e){
                 e.preventDefault();
 
-                self.open($(this).prop('href'));
+                self.open($(this).prop('href'), $(this).attr('data-article-id'), function(articleId) {
+                	var article = $.parseJSON(sessionStorage.getItem('articles' + articleId));
+                	article.content = $('.modal-body').html();
+
+                	self.db.collection('articles', article);
+                	sessionStorage.setItem('articles' + articleId, JSON.stringify(article));
+
+                	self.tinker.repost();
+                });
             });
         },
         detail: function(){
@@ -290,41 +317,48 @@ function Page() {
         }
     };
 
-    this.open = function(url) {
-        $.ajax({
-            url: url,
-            type: 'GET',
-            dataType: 'HTML',
-            beforeSend: function(xhr){
-                xhr.overrideMimeType('text/plain; charset=gbk');
-            }
-        }).done(function(data){
-            if (data) {
-                // remove all js
-                data = data.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
-                // remove head
-                data = data.replace(/<head\b[^<]*(?:(?!<\/head>)<[^<]*)*<\/head>/gi, '');
-                // remove html/body tag
-                data = data.replace(/.*?<(!DOCTYPE|body|html).*?>/gi, '');
+    this.open = function(url, articleId, callback) {
+    	var article = $.parseJSON(sessionStorage.getItem('articles' + articleId));
 
-                data = data.replace(/onload=".*?"/gi, '');
+    	if (article.content != '') {
+    		$('.modal-body').html(article.content);
+    	} else {
+    		$.ajax({
+	            url: url,
+	            type: 'GET',
+	            dataType: 'HTML',
+	            beforeSend: function(xhr){
+	                xhr.overrideMimeType('text/plain; charset=gbk');
+	            }
+	        }).done(function(data){
+	            if (data) {
+	                // remove all js
+	                data = data.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+	                // remove head
+	                data = data.replace(/<head\b[^<]*(?:(?!<\/head>)<[^<]*)*<\/head>/gi, '');
+	                // remove html/body tag
+	                data = data.replace(/.*?<(!DOCTYPE|body|html).*?>/gi, '');
 
-                var html = $(data);
+	                data = data.replace(/on(load|click)=".*?"/gi, '');
 
-                $('.modal-body').html(html.find('form').html());
-                self.tinker.common();
-                self.settings.inPage = true;
+	                var html = $(data);
 
-                self.tinker.detail();
-            }
+	                $('.modal-body').html(html.find('form').html());
+	                self.tinker.common();
+	                self.settings.inPage = true;
 
-            $('.modal-dialog').css({
-                'width': '844px'
-            });
-            $('#modalContainer').modal();
+	                self.tinker.detail();
+	                callback(articleId);
+	            }
+	        });
+    	}
+
+    	$('.modal-dialog').css({
+            'width': '844px'
         });
 
-    }
+        $('#modalContainer').modal();
+    };
 }
 
 function DataProvider() {
@@ -337,7 +371,8 @@ function DataProvider() {
         registry: {
         	collection: {},
         	collectionCount: 0
-        }
+        },
+        session: true 
     };
 
     this.push = function(key, value) {
@@ -349,7 +384,12 @@ function DataProvider() {
     this.collection = function(name, value) {
     	if (self.hasCollection(name)) {
             if ( ! self.hasCollectionEntry(name, value)) {
+            	if (self.settings.session) {
+            		sessionStorage.setItem(name + value.id, JSON.stringify(value));
+            	}
+            	
                 self.settings.data[name].push(value);
+
                 self.settings.registry.collection[name].entry += 1;
                 // for fast index search
                 self.settings.registry.collection[name].ids.push(value.id);
@@ -429,3 +469,90 @@ function DataProvider() {
         console.log(self.settings.registry);
     };
 }
+
+
+/*!
+ * jQuery Byllboard JavaScript Library v1.0.0
+ * http://talarist.com/byllboard
+ *
+ * @author: cyveros
+ * Released under the LGPL license
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ */
+
+;(function($){
+	$.fn.byllboard = function(options){
+		var configs = $.extend({
+			//hook: '.jobHolder',
+			pos: {
+                "0": {"left": 0, "top": 0},
+                "1": {"left": 300, "top": 0},
+                "2": {"left": 600, "top": 0},
+                "3": {"left": 900, "top": 0}
+			},
+			adjustment: 16
+		}, options);
+
+
+
+		var methods = {
+			get_min_top: function(){
+            	var val = configs.pos['0']['top'];
+            	var ref = "0";
+           
+           		$.each(configs.pos, function(key, value){
+                    	if (value['top'] < val){
+                            	val = value['top'];
+                            	ref = key;
+                    	}
+            	});
+               
+                return ref;
+			},
+			get_max_top: function(){
+            	var val = configs.pos['0']['top'];
+            	var ref = "0";
+           
+           		 $.each(configs.pos, function(key, value){
+                    	if (value['top'] > val){
+                            	val = value['top'];
+                            	ref = key;
+                    	}                      
+           		 });
+			
+            	return ref;        
+			},
+			get_min_left: function(){
+            	var val = configs.pos['0']['left'];
+            	var ref = "0";
+           
+           		 $.each(configs.pos, function(key, value){
+                    	if (value['left'] < val){
+                            	val = value['left'];
+                            	ref = key;
+                    	}
+            	});
+           
+            	return ref;
+            }               
+		
+		}; 
+		
+		return this.each(function(index){
+			var height =  $(this).height() + 'px';
+            $(this).css('height', height);
+           
+            var ref = methods.get_min_top();
+
+            $(this).css({
+                    'top': configs.pos[ref]['top'] + 'px',
+                    'left': configs.pos[ref]['left'] + 'px'
+            });
+           
+            configs.pos[ref]['top'] += (Number($(this).height()) + configs.adjustment);
+			
+			// $(configs.hook).css('height', Number(configs.pos[methods.get_max_top()]['top']) + 'px');
+		});
+	};
+})(jQuery);
